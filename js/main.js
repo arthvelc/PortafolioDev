ad6ccca839d33fc013c7a547020951ec6d982083
arthvelc <asolares.devml@gmail.com>
2024-06-22 18:17:00 -0600

    ya qudó la primera sección

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const swicthDarkLigth = document.querySelector('#themeToggle');
 const mainContainer = document.querySelector('.main-container');
 const texts = document.getElementsByClassName('text');
 
+const socialLinks = document.querySelector('.social-links');
+
 //Function to change the theme of the page
 
 function changeTheme(event){
@@ -12,10 +14,14 @@ function changeTheme(event){
         for (let i = 0; i < texts.length; i++) {
             texts[i].setAttribute('style', 'color: var(--dark-color)');
         }
+
+        socialLinks.setAttribute('style', 'background-color: #0c0c0ca6');
+
+        
     } else {
         mainContainer.setAttribute('style', 'background-color: var(--dark-color)');
         for (let i = 0; i < texts.length; i++) {
-            texts[i].setAttribute('style', 'color: var(--lig    -color)');
+            texts[i].setAttribute('style', 'color: var(--light-color)');
         }
     }
 
